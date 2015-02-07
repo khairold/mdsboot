0d4c187d2b8cb8753ddfc3bd3083e9fc9fb24fce
Khairold Safri Ibrahim <khairold@gmail.com>
2015-02-08 00:28:05 +0800

    html static

--- app/app.js
+++ app/app.js
@@ -3,20 +3,31 @@
 var app = angular.module('mdsApp', ['ui.bootstrap']);
 
 app.controller('CarouselDemoCtrl', function ($scope) {
-    $scope.myInterval = 5000;
+    $scope.myInterval = 7000;
     var slides = $scope.slides = [];
-    $scope.addSlide = function() {
-        var newWidth = 600 + slides.length + 1;
-        slides.push({
-            image: 'http://placekitten.com/' + newWidth + '/300',
-            text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-            ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-        });
-    };
-    for (var i=0; i<4; i++) {
-        $scope.addSlide();
-    }
 
-
-
+    slides.push({ "image": "images/carousel/1.png", "text": "More Cats", "active": false });
+    slides.push({ "image": "images/carousel/2.png", "text": "Extra Kittys", "active": true });
+    slides.push({ "image": "images/carousel/3.png", "text": "Lots of Felines", "active": false });
+    slides.push({ "image": "images/carousel/4.png", "text": "Surplus Cutes", "active": false });
 });
+
+app.controller('VideoCtrl', function ($scope, $octoshape) {
+    var player_id = "player";
+
+    var player_width = 1600;
+    var player_height = 900;
+
+    var player_streams = [
+        {
+            id: 'my_stream',
+            stream: 'octoshape://cdnstreams.tm.net.my/tmg/udrive/auto/Aminah',
+            eventStart: 1331725805000
+        }
+    ];
+    var player_stream = "my_stream";
+
+    var params = {allowFullScreen: true, scale: 'noscale', allowScriptAccess: 'always'};
+    var attributes = {id: player_id, name: player_id};
+});
+
---
+++ app/octoshape.js
@@ -0,0 +1,19 @@
+<script type="text/javascript" src="https://d2iwzbhgddzdwl.cloudfront.net/resources/player/infinitehd2/swfobject.js"></script>
+<script type="text/javascript">
+var player_id = "player";
+var player_width = 512;
+var player_height = 288;
+
+var player_streams = [
+    {
+        id: 'my_stream',
+        stream: 'octoshape://cdnstreams.tm.net.my/tmg/udrive/auto/Aminah',
+        eventStart: 1331725805000
+    }
+];
+var player_stream = "my_stream";
+
+var params = {allowFullScreen: true, scale: 'noscale', allowScriptAccess: 'always'};
+var attributes = {id: player_id, name: player_id};
+swfobject.embedSWF(document.location.protocol+'//d2iwzbhgddzdwl.cloudfront.net/resources/player/infinitehd2/player.swf', player_id, player_width, player_height, "10.2.0", null, null, params, attributes);
+</script>
